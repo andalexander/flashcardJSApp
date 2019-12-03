b8d3936002dbe3e68d44de6ff2ed87d7aef85a2d
Andrew Alexander <aarubesa@gmail.com>
2019-12-03 12:32:33 -0500

    Updates

---
+++ precache-manifest.ef50ba73f4e685cdd5d22078b3a0cf39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4addfe77cef8ddc193421d4c3b69b82",
+    "url": "/andrewRubesaProject5/index.html"
+  },
+  {
+    "revision": "41a369a96ba8b7411c0b",
+    "url": "/andrewRubesaProject5/static/css/main.d4b0c8e7.chunk.css"
+  },
+  {
+    "revision": "1b6480d045921ad1246e",
+    "url": "/andrewRubesaProject5/static/js/2.fc1c77c1.chunk.js"
+  },
+  {
+    "revision": "41a369a96ba8b7411c0b",
+    "url": "/andrewRubesaProject5/static/js/main.2145e5c7.chunk.js"
+  },
+  {
+    "revision": "47fa94ab9a4d3d931c76",
+    "url": "/andrewRubesaProject5/static/js/runtime-main.bfb474f3.js"
+  }
+]);
